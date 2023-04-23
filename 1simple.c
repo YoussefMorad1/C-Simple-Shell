81f687b6e1a7dbaa363e3e3c684eec435563ec97
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-23 23:00:09 +0200

    fullpush

---
+++ 1simple.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - hi
+ *
+ * Return: hi
+ */
+
+int main(void)
+{
+	while(1)
+	{
+		display_prompt();
+		s = getline();
+		
+	}
+}
