a120d0dc85b5fbf749f42d1be4eb51b53ba5397b
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-28 01:16:01 +0200

    fullpush

--- simple_shell.c
+++ simple_shell.c
@@ -13,51 +13,46 @@ int main(int argc, char **argv)
 	char *s = malloc(SIZE), **argv2, *cmd;
 
 	(void)argc;
+	errno = 0;
 	while (1)
 	{
 		argv2 = malloc(sizeof(*argv2)), cmd = NULL, argv2[0] = 0;
 		if (!is_pipe)
 			display_prompt();
 		if (!get_input(&s, &SIZE, argv2))
-			exit(stts);
+			exit(errno);
 		parse_input(s, &cmd, &argv2);
 		if (!_strlen(s) || !cmd || !_strlen(cmd))
 		{
 			clean_args(argv2);
 			continue;
 		}
+		stts = errno;
 		is_file = find_file(&cmd, _getenv("PATH"));
+		errno = stts;
 		if (is_file == -1 || !_strcmp(cmd, "exit"))
 		{
 			/*printint(stts);
 			printstr("\n");
 			printint(errno);
 			printstr("\n");*/
-			if (stts)
-				stts = errno;
 			clean_strs(s, cmd, 0);
 			if (is_file != -1)
-				exit_shell(argv2, argv, stts);
-			stts = errno;
+				exit_shell(argv2, argv, errno);
 			clean_args(argv2);
-			exit(stts);
+			exit(errno);
 		}
 		else if (!is_file)
 		{
 			printers(argv[0]);
 			printers(": No such file or directory\n");
 			execve(cmd, argv2, environ);
-			stts = errno;
 		}
 		else if (fork())
-		{
-			wait(&stts);
-		}
+			wait(0);
 		else
-		{
 			execve(cmd, argv2, environ);
-			stts = errno;
-		}
+		
 		clean_args(argv2);
 		clean_strs(cmd, 0, 0);
 	}
